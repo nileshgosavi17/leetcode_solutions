92fec99e000cd3ea671c8d66bddb3e3bed60837a
nileshgosavi17 <gosavi.nilesh.365@hmail.com>
2022-01-13 09:30:04 +0530

    Trial

--- Remove_Element.java
+++ Remove_Element.java
@@ -50,3 +50,14 @@ Constraints:
 0 <= val <= 100
 
 */
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int index=0;
+        for(int i:nums){
+            if(i !=val){
+                nums[index++]=i;
+            }
+        }
+        return index;
+    }
+}
