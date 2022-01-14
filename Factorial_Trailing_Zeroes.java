927c0583a84c9cb15088494c328d68ceef257999
Nilesh Gosavi <60322098+nileshgosavi17@users.noreply.github.com>
2022-01-14 20:35:08 +0530

    Update Factorial_Trailing_Zeroes.java

--- Factorial_Trailing_Zeroes.java
+++ Factorial_Trailing_Zeroes.java
@@ -3,15 +3,16 @@ Given an integer n, return the number of trailing zeroes in n!.
 
 Follow up: Could you write a solution that works in logarithmic time complexity?*/
 
+
 class Solution {
     public int trailingZeroes(int n) {
-        int count=0;
+        int sum=0;
         while(n>0)
         {
             n /=5;
-            count +=n;
+            sum +=n;
             
         }
-        return count;
+        return sum;
     }
 }
